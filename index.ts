95ec124a84ba75b7fa53fa6d74ef4a7fcf743d5b
ASAD AMEER <160565964+asadkhaaan@users.noreply.github.com>
2024-04-02 08:29:56 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+
+// computer will generate a random number. Done
+
+// user input for guessing number. Done
+
+// compare user input with computer generated number show result!!!. Done
+// Math.random mean only 0 to 1 in only points values so we use Math.floor can lowest number
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+
+
+const answers = await inquirer.prompt([
+    {
+    name: "userGuessedNumber",
+    message: "please guess a number between 1 - 6",
+    type: "number",
+},
+]);
+
+if(answers.userGuessedNumber === randomNumber) {
+    console.log("congratulations! you guessed right number.");
+}else { 
+    console.log("you guessed wrong number.");
+}
